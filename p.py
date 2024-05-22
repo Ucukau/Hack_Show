16aad857a91bf4c2ce448b4ebfc0127d6871d999
Radita Aditya <ptbp321@gmail.com>
2024-05-22 21:53:32 +0800

    Create p.py

---
+++ p.py
@@ -0,0 +1,36 @@
+
+# HEH LU YANG BACA KAYAK KONTOL
+# MAU APA LU HAH
+# INI NIH CODINGNYA KALO MAU AMBIL TINGGAL COPAS AJA
+# HABIS INI BIKIN SENDIRI LAH 
+
+import time
+import os 
+import random
+from colorama import init, Fore, Back, Style
+
+def hack_green_run():
+    for i in range(1, 1000):
+      print(Fore.GREEN + "   0   " + "    " + "   0  " + "      " + "   0    " + "      " + "   0   " + * i)
+      print(Fore.GREEN + "  2    " + "    " + "  8   " + "      " + "  5     " + "      " + "  9    " + * i)
+      print(Fore.GREEN + "   0   " + "    " + "   0  " + "      " + "   0    " + "      " + "   0   " + * i)
+      print(Fore.GREEN + "    7  " + "    " + "    4 " + "      " + "    1   " + "      " + "    6  " + * i)
+      print(Fore.GREEN + "   0   " + "    " + "   5  " + "      " + "   0    " + "      " + "   3   " + * i + Style.RESET_ALL)
+      time.sleep(0.10)
+hack_green_run()
+
+def welcome():
+    print(Fore.YELLOW + "Sudah Selesai, Jika ingin yang lain bisa berikan ide yang menarik dan kren tentunya.)
+    print("Hubungi Di Nomor = +628781695079")
+    print(Back.GREEN + Fore.WHITE + "Author = Radita_Aditya")
+    print("Leangue = Python" + Style.RESET_ALL)
+    time.sleep(1)
+
+def donasi():
+    print(Fore.YELLOW + "Berhubung Mimin Baik, Scriptnya" + Fore.GREEN + "GRATIS" + Fore.YELLOW + "Jadi Kalian Boleh CoPas, Tapi Mimin Butuh Dana Buat beli Kouta, Boleh dong Donasi")
+    print(Fore.WHITE + Back.GREEN + "DONASI == +62877144001 [GOPAY, OPO]" + Style.RESET_ALL)
+  
+          
+
+welcome()
+donasi()
